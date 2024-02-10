60cf6e08f1e526701431e37c6bf87343ce8a8cee
Implycitt <qgbordelon@gmail.com>
2024-02-10 11:21:37 -0600

    project initialized

---
+++ src/main/java/com/Implycitt/App.java
@@ -0,0 +1,13 @@
+package com.Implycitt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
